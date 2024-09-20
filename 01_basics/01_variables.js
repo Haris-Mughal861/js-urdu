1c8f5e35832b9568c95f2db0deda210ece1afefd
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-09-20 10:55:56 +0000

    01_variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "harismughal861@gmail.com"
+var accountPassword = "12345"
+accountCity = "Quetta"
+
+//accountId = 2
+accountEmail = "fsjaijfjf"
+accountPassword = "886448"
+accountCity = "hsdidk"
+console.log(accountId);
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
