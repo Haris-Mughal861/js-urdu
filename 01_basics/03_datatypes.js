fe33bd6d55a2d6a4806b523f100d9e053e899e57
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-09-25 17:51:08 +0000

    DataTypes2

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,34 @@
+// refrence (Premitive)
+
+const score = 100
+const scoreValue = 100.9
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 123456789123456789n
+
+console.log(id===anotherId);
+
+
+
+console.log("Harismughal");
+
+
+// Refrence (Non Premitives)
+
+const names = ["haris", "Mughal", "Shahid"]
+let myObj = {
+    name: "Haris",
+    age : 21,
+}
+
+const myFuction = function(){
+    console.log("Hello World");
+    
+}
+console.log(myFuction);
