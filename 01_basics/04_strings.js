88db5b5fc7242acf3096bf6c6989bdefd13363b1
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-09-28 19:23:25 +0000

    Strings practices

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -5,3 +5,30 @@ const repoCount = 50
 //console.log(name + repoCount +  "value");
 
 console.log("Hellow my name is ${name} and repocount is ${repoCount}");
+
+const gameName = new String("Haris")
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("i"));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice (-8, 4)
+console.log(anotherString);
+
+const newStringOne = " Haris   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+const url = "http://haris.com/haris861mughal"
+
+console.log(url.replace('861' , '874'));
+
+
+
