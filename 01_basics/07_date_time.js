9ba7900489c099e16ce6153d0471af9342992b73
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-10-02 16:53:08 +0000

    Date and Tme

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,38 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toLocaleTimeString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date (1-04-2015)
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStam = Date.now()
+console.log(myCreatedDate.getTime());
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+
+newDate.toLocaleString('default'{
+    weekday: 'long',
+    timeZone: ''
+})
+
+
