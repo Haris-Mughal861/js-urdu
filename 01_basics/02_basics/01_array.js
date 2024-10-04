7166bceac943c13d428cf3175540a54f1788145d
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-10-04 18:00:21 +0000

    Array

--- 01_basics/02_basics/01_array.js
+++ 01_basics/02_basics/01_array.js
@@ -35,7 +35,61 @@ const myn1 = myArr.slice(1,3)
 console.log(myn1);
 console.log("b", myArr);
 
+// refrence (Premitive)
 
+const score = 100
+const scoreValue = 100.9
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 123456789123456789n
+
+console.log(id===anotherId);
+
+
+
+console.log("Harismughal");
+
+
+// Refrence (Non Premitives)
+
+const names = ["haris", "Mughal", "Shahid"]
+let myObj = {
+    name: "Haris",
+    age : 21,
+}
+
+const myFuction = function(){
+    console.log("Hello World");
+    
+}
+console.log(myFuction);
+
+
+
+
+//Stack(primitive) and Heap(non Primitive)
+
+let myName = "Haris" //Stack
+
+let anotherName = myName
+anotherName = "Mughal"
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    password: "user@bl"
+}
+let userTwo = userOne
+userTwo.email = "Harismughal@123"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
