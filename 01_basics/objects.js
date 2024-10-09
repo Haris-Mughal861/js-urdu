ebae758e2a2c510ffe7037cd55fed9608e48f158
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-10-09 17:16:33 +0000

    Objects_js

---
+++ 01_basics/objects.js
@@ -0,0 +1,16 @@
+// singleton
+
+const JsUser = {
+    name: "harismughal",
+    age: 21,
+    location: "Quetta",
+    email: "harismughal861@gmail.com",
+    isLoggedIn: false,
+    lastLogedinDays: ["Monday", "Saturday"],
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"])
+console.log(JsUser.age);
+
+
