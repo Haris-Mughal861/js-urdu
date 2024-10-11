11ffc8531be261cbf8274ea8862a7b796aad1928
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-10-11 15:29:35 +0000

    objects

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -24,4 +24,34 @@ JsUser.greeting = function(){
 console.log(JsUser.greeting());
 
 
+const tinderUser = new Object()
+
+tinderUser.id = "123456"
+
+tinderUser.name = "Haris"
+
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const ragularUser ={
+    email: "harismughal@123",
+    fullname:{
+        userfullname:{
+            firstname: "muhammad",
+            lastname: "haris",
+
+        }
+    }
+}
+console.log(ragularUser.fullname.firstname);
+
+
+const obj1 = {1: "asd", 2: "b"}
+
+const obj2 = {3: "erty", 4: "bv"}
+
+const obj3 = Object.assign(obj1,obj2)
+console.log(obj3);
+
 
