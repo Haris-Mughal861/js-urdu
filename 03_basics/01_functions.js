6b0e3a69d6a6ec35020c92f586247b2752e549eb
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-10-15 16:24:31 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,13 +39,44 @@ function loginUserMassage(username){
     if(username === undefined)
         console.log("Please entre user name");
         
-    return '${username} just logged in'
+ //   return '${username} just logged in'
 
 }
- return '${username} just logged in'
+// return '${username} just logged in'
 
 //console.log(loginUserMassage("Harismughal"));
 console.log(loginUserMassage());
+
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(234,456,566));
+
+const user = {
+    username:"haris",
+    price: 2345
+}
+
+function handleObject(anyobject){
+    console.log('Username is ${anyobject.username} and price is ${anyobject.price}');
+    
+
+}
+
+handleObject({
+    username:"haris",
+    price :234
+})
+const myNewarray = [23,234,456,456,675]
+
+function returnSecoundvalue(getarray){
+    return getarray[1]
+}
+
+console.log(returnSecoundvalue(myNewarray));
+
 
 
 
