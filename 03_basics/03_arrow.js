cfc7226a50c0fae6e0c676722873b3e3a093030b
Muhammad Haris <144819304+Haris-Mughal861@users.noreply.github.com>
2024-10-19 18:19:04 +0000

    Arrows

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,25 @@
+const user ={
+    username:"haris",
+    price: 999,
+
+    welcomeMesage: function(){
+        console.log('$(this.username) ,welcome to website');
+        console.log(this);
+        
+        
+    }
+}
+
+user.welcomeMesage()
+
+console.log(this);
+
+
+function code(){
+
+    let username = "haris"
+    console.log(this.username);
+    
+}
+
+code()
